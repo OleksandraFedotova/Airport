98277a8a873b6d1dd966636ceb1c4dc002c4748b
OleksandraFedotova <feyasasha19111996@gmail.com>
2018-07-13 19:21:19 +0300

    Added Modules to startup

--- Airport/Airport/Startup.cs
+++ Airport/Airport/Startup.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Airport.Implementation;
+using AirPort.DataAccess;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper.Configuration;
+using Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace Airport
 {
@@ -16,13 +18,27 @@ namespace Airport
             Configuration = configuration;
         }
 
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+            });
+            services.AddCors();
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule<InfrastructureModule>();
+            builder.RegisterModule<ImplementationModule>();
+            builder.RegisterModule<DataAccessModule>();
+
+            var container = builder.Build();
+
+            return new AutofacServiceProvider(container);
+        }
+
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
